558c3bede85dd49fb883748912ea2bc83d3e83a4
lruizap <lruizap2502@gmail.com>
2023-06-15 11:55:19 +0200

    Actualizado: "main.py" para añadir el grupo de valores "V-math"

--- Vitalia-BOT/main.py
+++ Vitalia-BOT/main.py
@@ -60,16 +60,94 @@ async def ping(ctx):
     await ctx.send("pong!")
 
 
-@Vitalia.command(
+@Vitalia.group(
+    aliases=['m'],
+    description='Vitalia tiene comandos para operaciones matemáticas',
+    help='Esta es la ayuda para el comando "V-math"',
+    brief='Vitalia te ayuda con las mates'
+)
+async def math(ctx):
+    usuario = ctx.message.author.mention
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'Por favor {usuario}, Introduzca un comando perteneciente al grupo')
+
+
+# ? Simple
+
+
+@math.group(
+    aliases=['s'],
+    description='Vitalia tiene comandos para operaciones matemáticas simples',
+    help='Esta es la ayuda para el comando "V-math simple"',
+    brief='Vitalia te ayuda con las mates'
+)
+async def simple(ctx):
+    usuario = ctx.message.author.mention
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'Por favor {usuario}, Introduzca un comando perteneciente al grupo')
+
+
+@simple.command(
     aliases=['+'],
     description='Vitalia suma dos numeros',
-    help='Esta es la ayuda para el comando "V-sum"',
+    help='Esta es la ayuda para el comando "V-math simple sum"',
     brief='Vitalia suma dos numeros'
 )
 #! Suma dos números
 async def sum(ctx, one: int, two: int):
     usuario = ctx.message.author.mention
     await ctx.send(f"{usuario}, el resultado es: {one + two}")
+
+
+@simple.command(
+    aliases=['-'],
+    description='Vitalia resta dos numeros',
+    help='Esta es la ayuda para el comando "V-math simple subtract"',
+    brief='Vitalia resta dos numeros'
+)
+#! Resta dos números
+async def subtract(ctx, one: int, two: int):
+    usuario = ctx.message.author.mention
+    await ctx.send(f"{usuario}, el resultado es: {one - two}")
+
+
+# ? Avanzado
+
+
+@math.group(
+    aliases=['a'],
+    description='Vitalia tiene comandos para operaciones matemáticas avanzadas',
+    help='Esta es la ayuda para el comando "V-math advanced"',
+    brief='Vitalia te ayuda con las mates'
+)
+async def advanced(ctx):
+    usuario = ctx.message.author.mention
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'Por favor {usuario}, Introduzca un comando perteneciente al grupo')
+
+
+@advanced.command(
+    aliases=['*'],
+    description='Vitalia multiplica dos numeros',
+    help='Esta es la ayuda para el comando "V-math avanced multiply"',
+    brief='Vitalia multiplica dos numeros'
+)
+#! Multiplica dos números
+async def multiply(ctx, one: int, two: int):
+    usuario = ctx.message.author.mention
+    await ctx.send(f"{usuario}, el resultado es: {one * two}")
+
+
+@advanced.command(
+    aliases=['/'],
+    description='Vitalia divide dos numeros',
+    help='Esta es la ayuda para el comando "V-math avanced divide"',
+    brief='Vitalia divide dos numeros'
+)
+#! Divide dos números
+async def divide(ctx, one: int, two: int):
+    usuario = ctx.message.author.mention
+    await ctx.send(f"{usuario}, el resultado es: {one / two}")
 
 
 @Vitalia.command(
@@ -92,11 +170,13 @@ async def helpCommands(ctx):
 
     # Ordenar en orden alfabético
     embed.add_field(
-        name="V-helpCommands", value="Vitalia nos dará una guia de sus comandos para que los usemos", inline=False)
+        name="`V-helpCommands`", value="Vitalia nos dará una guia de sus comandos para que los usemos", inline=False)
     embed.add_field(
-        name="V-hola", value="Vitalia saluda al usuario que envió el comando", inline=False)
+        name="`V-hola`", value="Vitalia saluda al usuario que envió el comando", inline=False)
     embed.add_field(
-        name="V-ping", value="Vitalia contesta con un 'pong!'", inline=False)
+        name="`V-math`", value="Vitalia tiene operaciones matemáticas\nConsultar con el comando `V-help math`", inline=False)
+    embed.add_field(
+        name="`V-ping`", value="Vitalia contesta con un 'pong!'", inline=False)
 
     embed.set_footer(text="Espero que te haya ayudado")
 
@@ -109,6 +189,30 @@ async def helpCommands(ctx):
 @sum.error
 #! Manejo de errores del comando "sum"
 async def sum_error(ctx, error):
+    usuario = ctx.message.author.mention
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Por favor, {usuario}, comprueba los valores introducidos")
+
+
+@subtract.error
+#! Manejo de errores del comando "subtract"
+async def subtract_error(ctx, error):
+    usuario = ctx.message.author.mention
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Por favor, {usuario}, comprueba los valores introducidos")
+
+
+@multiply.error
+#! Manejo de errores del comando "multiply"
+async def multiply_error(ctx, error):
+    usuario = ctx.message.author.mention
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Por favor, {usuario}, comprueba los valores introducidos")
+
+
+@divide.error
+#! Manejo de errores del comando "divide"
+async def divide_error(ctx, error):
     usuario = ctx.message.author.mention
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send(f"Por favor, {usuario}, comprueba los valores introducidos")
