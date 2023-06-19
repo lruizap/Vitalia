3642899505baae46596263e8162f639b9ec4f853
lruizap <lruizap2502@gmail.com>
2023-06-19 13:02:41 +0200

    Actualizado: Manejo de canales y mensajes privados

--- Vitalia-BOT/main.py
+++ Vitalia-BOT/main.py
@@ -7,7 +7,11 @@ from discord.ext import commands
 logger = settings.logging.getLogger("Vitalia")
 
 
-Vitalia = commands.Bot(command_prefix='V-', intents=discord.Intents.all(),
+intents = discord.Intents.all()
+intents.message_content = True
+intents.members = True
+
+Vitalia = commands.Bot(command_prefix='V-', intents=intents,
                        status=discord.Status.dnd,
                        activity=discord.Game(name="Programando BOT de DISCORD"))
 
@@ -39,11 +43,22 @@ async def on_ready():
 
 
 @Vitalia.event
+#! Evento para enviar un mensaje a usuarios nuevos del servidor
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hola, {member.mention}, Bienvenido a nuestro servidor de pruebas.\nMuchas gracias por ayudar')
+
+
+@Vitalia.event
 #! Evento para que imprime los mensajes enviados por pantalla
 async def on_message(message):
     # Verificar que el mensaje no sea del propio bot
     if message.author == Vitalia.user:
         return
+
+    # Replica al mensaje privado
+    if isinstance(message.channel, discord.channel.DMChannel) and message.author != Vitalia.user:
+        await message.reply('Lo siento, no estoy programado para ayudarte con lo que me pides')
 
     print(
         f"\033[34m\n 🧑 ► {message.author} ha dicho: {message.content}\n\033[39m")
@@ -77,7 +92,8 @@ async def reload(ctx, cog: str):
 #! Contesta al usuario con un "pong!"
 async def ping(ctx):
     # ctx es el canal donde se ha enviado el mensaje
-    await ctx.send("pong!")
+    # con ctx podemos hacer que el mensaje de respuesta lo de por privado
+    await ctx.message.author.send("pong!")
 
 
 """ //- ERRORES -// """
